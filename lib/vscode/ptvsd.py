572f3fd1dd0c37ff1896cb10251f9f4cb35ac3df
vd <vadyur@csodessa.com>
2020-08-28 18:15:14 +0300

    ++

--- lib/vscode/ptvsd.py
+++ lib/vscode/ptvsd.py
@@ -27,9 +27,61 @@ break_into_debugger = debugpy.breakpoint
 debug_this_thread = debugpy.debug_this_thread
 is_attached = debugpy.is_client_connected
 
+def launch_server(argv):
+    #import web_pdb
+    #web_pdb.set_trace()
+
+    import os
+    os.name = 'kodi'
+
+    from debugpy.adapter.__main__ import main, _parse_argv
+    main(_parse_argv(argv))
+
+    os.name = _name
+
+
+class MyPopen:
+    def __init__(self, *args, **kwargs):
+        import json
+        from debugpy.adapter.__main__ import main, _parse_argv
+        argv = args[0][1:]
+    	import xbmc
+        this_script = 'special://home/addons/script.module.vsdbg/lib/vscode/ptvsd.py'
+        params = ','.join(argv)
+    	xbmc.executebuiltin('RunScript({},launch_server,{})'.format(this_script, params))
+
+    def wait(self):
+        #import web_pdb
+        #web_pdb.set_trace()
+        import time
+        time.sleep(1)
+
 def enable_attach(address, *args, **kwargs):
+    #import web_pdb
+    #web_pdb.set_trace()
+
     import xbmc
     xbmc.log('enable_attach!!!')
+
+    import subprocess
+
+    _Popen = subprocess.Popen
+
+    subprocess.Popen = MyPopen
+
+    #debugpy.configure(subProcess=False)
     debugpy.listen(address)
 
+    subprocess.Popen = _Popen
+
 wait_for_attach = debugpy.wait_for_client
+
+if __name__ == '__main__':
+    if 'launch_server' in sys.argv:
+        import xbmc
+        xbmc.log(str(sys.argv))
+        pos = sys.argv.index('launch_server')
+        argv = sys.argv[pos+1:]
+        xbmc.log(str(argv))
+
+        launch_server(argv)
